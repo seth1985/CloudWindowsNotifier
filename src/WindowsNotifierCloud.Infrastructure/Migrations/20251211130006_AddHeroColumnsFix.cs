493fd86caa4347ebb1e8668669befcd67df8c5fa
seth1985 <crvenkovicbojan@gmail.com>
2025-12-11 14:00:58 +0100

    Fix missing hero columns migration

---
+++ src/WindowsNotifierCloud.Infrastructure/Migrations/20251211130006_AddHeroColumnsFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WindowsNotifierCloud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHeroColumnsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
